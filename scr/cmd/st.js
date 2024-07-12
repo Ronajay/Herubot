4eb61fc0d554333cdc9d9c384736d74507db8b2c
RonaZhen <zhenheru1@gmail.com>
2024-07-13 06:52:39 +0800

    Update st.js

--- scr/cmd/st.js
+++ scr/cmd/st.js
@@ -2,22 +2,24 @@ module.exports.config = {
   name: "st",
   accessableby: 0,
   author: "Deku",
-  description: "get sticker id",
+  description: "Get sticker ID",
   usage: "[reply]",
   prefix: true
-}
+};
 
 module.exports.start = async function ({ api, event, text }) {
-  if (event.type == "message_reply") {
-    if (event.messageReply.attachments[0].type == "sticker") {
+  if (event.type === "message_reply") {
+    const attachment = event.messageReply.attachments[0];
+    if (attachment && attachment.type === "sticker") {
       return api.sendMessage({
-        body: `ID: ${event.messageReply.attachments[0].ID}\nCaption: ${event.messageReply.attachments[0].description}`
-      }, event.threadID)
+        body: `ID: ${attachment.ID}\nCaption: ${attachment.description || 'No description'}`
+      }, event.threadID, event.messageID);
+    } else {
+      return api.sendMessage("Please reply to a sticker.", event.threadID, event.messageID);
     }
-    else return api.sendMessage("Just reply to the sticker.", event.threadID);
+  } else if (text[0]) {
+    return api.sendMessage({ sticker: text[0] }, event.threadID, event.messageID);
+  } else {
+    return api.sendMessage("Please reply to a sticker or provide a sticker ID.", event.threadID, event.messageID);
   }
-  else if (text[0]) {
-    return api.sendMessage({ sticker: text[0] }, event.threadID);
-  }
-  else return api.sendMessage("Just reply to the sticker.", event.threadID);
-}
+};
