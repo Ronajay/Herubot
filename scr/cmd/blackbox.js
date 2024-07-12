7a37c5711a680fb826a91cff2e7fb4667ceadd71
RonaZhen <zhenheru1@gmail.com>
2024-07-12 19:55:53 +0800

    Update blackbox.js

--- scr/cmd/blackbox.js
+++ scr/cmd/blackbox.js
@@ -10,7 +10,7 @@ module.exports = {
     let p = text.join(' '), uid = event.senderID;
     const axios = require('axios');
     if (!p) return reply('Please enter a prompt.');
-    react('✨');
+    react('🖤');
     try {
       const r = (await axios.get(`https://joshweb.click/api/blackboxai?q=${p}&uid=${uid}`)).data;
       return reply("✅ + 𝙱𝚕𝚊𝚌𝚔𝚋𝚘𝚡 𝙰𝙸 𝙲𝚘𝚗𝚝𝚒𝚗𝚞𝚎𝚜 𝙲𝚘𝚗𝚟𝚎𝚛𝚜𝚊𝚝𝚒𝚘𝚗𝚊𝚕\n━━━━━━━━━━━━━━━━━━\n" + r.result + "\n━━━━━━━━━━━━━━━━━━\nType “blackbox clear” if you want to clear the conversations");
