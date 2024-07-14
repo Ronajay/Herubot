689be77593cdcdcaf50f34713c63b02efc9a8540
RonaZhen <zhenheru1@gmail.com>
2024-07-14 11:34:36 +0800

    Update imgur.js

--- scr/cmd/imgur.js
+++ scr/cmd/imgur.js
@@ -1,56 +1,41 @@
 const axios = require("axios");
 
-class Imgur {
-  constructor() {
-    this.clientId = "fc9369e9aea767c";
-    this.client = axios.create({
-      baseURL: "https://api.imgur.com/3/",
-      headers: {
-        Authorization: `Client-ID ${this.clientId}`
+module.exports.config = {
+  name: "imgur",
+  accessableby: 0,
+  author: "Deku",
+  description: "Upload a photo to Imgur and return the link",
+  usage: "[reply to a photo]",
+  prefix: true
+};
+
+module.exports.start = async function ({ api, event }) {
+  const clientId = "fc9369e9aea767c"; // Replace with your actual Imgur client ID
+
+  if (event.type !== "message_reply" || !event.messageReply.attachments.length || event.messageReply.attachments[0].type !== "photo") {
+    return api.sendMessage("Please reply to a photo that you want to upload to Imgur.", event.threadID, event.messageID);
+  }
+
+  const imageUrl = event.messageReply.attachments[0].url;
+
+  try {
+    const response = await axios.post(
+      "https://api.imgur.com/3/image",
+      {
+        image: imageUrl,
+      },
+      {
+        headers: {
+          Authorization: `Client-ID ${clientId}`
+        }
       }
-    });
+    );
+
+    const imgurLink = response.data.data.link;
+
+    return api.sendMessage(`Uploaded successfully: ${imgurLink}`, event.threadID, event.messageID);
+  } catch (error) {
+    console.error(`Error uploading to Imgur: ${error.message}`);
+    return api.sendMessage("Failed to upload the photo to Imgur. Please try again later.", event.threadID, event.messageID);
   }
-
-  async uploadImage(url) {
-    const response = await this.client.post("image", {
-      image: url
-    });
-    return response.data.data.link;
-  }
-}
-
-class Modules extends Imgur {
-  constructor() {
-    super();
-  }
-
-  get config() {
-    return {
-      name: "imgur",
-      accessableby: 0,
-      description: "Upload to imgur",
-      usage: "[reply]",
-      prefix: false,
-    };
-  }
-
-  start = async function ({ api, event }) {
-    if (event.type !== "message_reply" || event.messageReply.attachments.length === 0) {
-      return api.sendMessage("Please reply with the photo/video/gif that you need to upload", event.threadID, event.messageID);
-    }
-
-    const array = [];
-    for (let { url } of event.messageReply.attachments) {
-      try {
-        const res = await this.uploadImage(url);
-        array.push(res);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-    return api.sendMessage(`Uploaded successfully ${array.length} image(s)\nFailed to upload: ${event.messageReply.attachments.length - array.length}\nImage link:\n${array.join("\n")}`, event.threadID, event.messageID);
-  }
-}
-
-module.exports = new Modules();
+};
